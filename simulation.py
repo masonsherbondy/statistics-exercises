cf896b6a400a497682e09f638e06ea9e2daa0c23
masonsherbondy <sherbondy.mason@gmail.com>
2021-11-02 18:54:08 -0500

    clean edit

--- simulation.py
+++ simulation.py
@@ -9,7 +9,8 @@ import numpy as np
 import pandas as pd
 
 
-# Exercise 1. How likely is it that you roll doubles when rolling two dice?
+# ## Exercise 1
+# How likely is it that you roll doubles when rolling two dice?
 
 # In[2]:
 
@@ -32,7 +33,7 @@ doubles.mean()
 #there is a 17% chance that you will roll doubles when rolling 2 dice
 
 
-# # Global Variable
+# ## global variable n_trials
 
 # In[5]:
 
@@ -40,7 +41,8 @@ doubles.mean()
 n_trials = 1_000_000
 
 
-# Exercise 2. If you flip 8 coins, what is the probability of getting exactly 3 heads? What is the probability of getting more than 3 heads?
+# ## Exercise 2
+# If you flip 8 coins, what is the probability of getting exactly 3 heads? What is the probability of getting more than 3 heads?
 
 # In[6]:
 
@@ -89,7 +91,7 @@ coins.head()
 #there is a 64% probability you will get more than 3 heads when you flip 8 coins
 
 
-# Exercise 3. 
+# ## Exercise 3
 # There are approximitely 3 web development cohorts for every 1 data science cohort at Codeup. Assuming that Codeup randomly selects an alumni to put on a billboard, what are the odds that the two billboards I drive past both have data science students on them?
 
 # In[13]:
@@ -118,11 +120,13 @@ picks[picks.sum(axis = 1) == 2]
 
 
 picks[picks.sum(axis = 1) == 2].shape[0] / n_trials
+#there is a 6% chance that both billboards feature a data scientist alumni
 
 
 # In[17]:
 
 
+#alternatively
 picks['codeup'] = picks.sum(axis = 1)
 
 
@@ -138,10 +142,11 @@ picks.head()
 ds_prob = (picks.codeup == 2).mean()
 odds = ds_prob / (1 - ds_prob)
 1 / odds
-#the odds are 1 to 15 that both billboards will feature data science students
+#the odds are about 1 to 15 that both billboards will feature data science students
 
 
-# Exercise 4. Codeup students buy, on average, 3 poptart packages with a standard deviation of 1.5 a day from the snack vending machine. If on monday the machine is restocked with 17 poptart packages, how likely is it that I will be able to buy some poptarts on Friday afternoon? (Remember, if you have mean and standard deviation, use the np.random.normal)
+# ## Exercise 4
+# Codeup students buy, on average, 3 poptart packages with a standard deviation of 1.5 a day from the snack vending machine. If on monday the machine is restocked with 17 poptart packages, how likely is it that I will be able to buy some poptarts on Friday afternoon? (Remember, if you have mean and standard deviation, use the np.random.normal)
 
 # In[20]:
 
@@ -169,9 +174,10 @@ consumed.poptarts_consumed <= 16
 
 
 (consumed.poptarts_consumed <= 16).mean()
+#there is an 88% chance you will be able to buy poptarts on a Friday afternoon
 
 
-# Exercise 5. Compare Heights
+# ## Exercise 5 Compare Heights
 # 
 # Men have an average height of 178 cm and standard deviation of 8cm.
 # Women have a mean of 170, sd = 6cm.
@@ -220,7 +226,8 @@ women_taller_than_men.mean()
 #There is a 21% that a woman is taller than a man when both are chosen at random
 
 
-# Exercise 6. When installing anaconda on a student's computer, there's a 1 in 250 chance that the download is corrupted and the installation fails. What are the odds that after having 50 students download anaconda, no one has an installation issue? 100 students?
+# ## Exercise 6
+# When installing anaconda on a student's computer, there's a 1 in 250 chance that the download is corrupted and the installation fails. What are the odds that after having 50 students download anaconda, no one has an installation issue? 100 students?
 # 
 # What is the probability that we observe an installation issue within the first 150 students that download anaconda?
 # 
@@ -243,8 +250,11 @@ no_issues.sum(axis = 1) == 0
 # In[32]:
 
 
-(no_issues.sum(axis = 1) == 0).mean()
+issue_rate = (no_issues.sum(axis = 1) == 0).mean()
 #There is an 82% chance that there will be no issues if 50 students download anaconda
+odds = issue_rate / (1 - issue_rate)
+odds
+#the odds that there will be no issues are about 9 to 2
 
 
 # In[33]:
@@ -258,8 +268,11 @@ no_issues = pd.DataFrame(no_issues)
 # In[34]:
 
 
-(no_issues.sum(axis = 1) == 0).mean()
+issue_rate = (no_issues.sum(axis = 1) == 0).mean()
 #There is a 67% chance that there will be no issues if 100 students download anaconda
+odds = issue_rate / (1 - issue_rate)
+odds
+#the odds that there will be no issues in 100 downloads are about 2 to 1
 
 
 # In[35]:
@@ -289,10 +302,10 @@ no_issues = pd.DataFrame(no_issues)
 
 
 (no_issues.sum(axis = 1) == 0).mean()
-#there is a 16% chance there will be no issues if 450 students download anaconda
+#there is a 17% chance there will be no issues if 450 students download anaconda
 
 
-# Exercise 7.
+# ## Exercise 7
 # There's a 70% chance on any given day that there will be at least one food truck at Travis Park. However, you haven't seen a food truck there in 3 days. How unlikely is this?
 # 
 # How likely is it that a food truck will show up sometime this week?
@@ -329,7 +342,8 @@ food_truck
 #There is a 100% chance that a food truck will show up sometime this week
 
 
-# Exercise 8. If 23 people are in the same room, what are the odds that two of them share a birthday? What if it's 20 people? 40?
+# ## Exercise 8
+# If 23 people are in the same room, what are the odds that two of them share a birthday? What if it's 20 people? 40?
 
 # In[43]:
 
@@ -350,7 +364,8 @@ birthdays.nunique(axis = 1) < 23
 
 
 (birthdays.nunique(axis = 1) < 23).mean()
-#the odds that 2 people in the same room of 23 people share a birthday is about 1 to 1
+#there is a 51% chance of finding 2 people with the same birthday in the same room of 23 people 
+#the odds that 2 people in the same room of 23 people share a birthday are 51 to 49 (about 1 to 1)
 
 
 # In[46]:
@@ -365,7 +380,7 @@ birthdays = pd.DataFrame(birthdays)
 
 
 (birthdays.nunique(axis = 1) < 20).mean()
-#the odds that 2 people in the same room of 20 people share a birthday is about 2 to 3
+#the odds that 2 people in the same room of 20 people share a birthday are 41 to 59 (about 2 to 3)
 
 
 # In[48]:
@@ -380,5 +395,5 @@ birthdays = pd.DataFrame(birthdays)
 
 
 (birthdays.nunique(axis = 1) < 40).mean()
-#the odds that 2 people in the same room of 40 people share a birthday is about 9 to 1
+#the odds that 2 people in the same room of 40 people share a birthday are 89 to 11 (about 9 to 1)
 
